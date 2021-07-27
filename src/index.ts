2b0876ec5efddfb36702818cc334f718a105b892
rjrunner44 <rob@adivate.net>
2021-07-27 07:47:35 -0700

    updates

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,10 @@ const token = process.env['BOT_TOKEN'];
 if (token === undefined) {
   throw new Error('BOT_TOKEN must be provided!');
 }
+const webhookUrl = process.env['WEBHOOK_URL'];
+if (webhookUrl === undefined) {
+  throw new Error('WEBHOOK_URL must be provided!');
+}
 
 const bot = new Telegraf(token);
 // Set the bot response
@@ -31,6 +35,7 @@ const secretPath = `/telegraf/${bot.secretPathComponent()}`;
 // Set telegram webhook
 // npm install -g localtunnel && lt --port 3000
 //bot.telegram.setWebhook(`https://----.localtunnel.me${secretPath}`);
+bot.telegram.setWebhook(webhookUrl);
 
 const app = express();
 app.get('/', (_: Request, res: Response) => res.send('Hello World!'));
