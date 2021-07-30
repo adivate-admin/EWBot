b2563ac695d57cbba9a7ab3e34a58631eab90dac
rjrunner44 <rob@adivate.net>
2021-07-30 08:02:24 -0700

    updates

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import express, { Request, Response } from 'express';
 import path from 'path';
-import { Telegraf } from 'telegraf';
+import { Telegraf, Markup } from 'telegraf';
 
 // pull configs from .env:
 const env = dotenv.config();
@@ -24,10 +24,20 @@ const port = process.env['PORT'] || 8443;
 // feedTheDragons();
 // startBot();
 
+const keyboard = Markup.inlineKeyboard([
+  Markup.button.url('❤️', 'http://telegraf.js.org'),
+  Markup.button.callback('Delete', 'delete'),
+]);
+
 const bot = new Telegraf(token);
 // Set the bot response
 bot.on('text', ctx => ctx.replyWithHTML('<b>Hello</b>'));
-
+bot.start(ctx => ctx.reply('Hello from EWBot'));
+bot.help(ctx => ctx.reply('Help from EWBot'));
+bot.on('message', ctx =>
+  ctx.telegram.sendCopy(ctx.message.chat.id, ctx.message, keyboard),
+);
+bot.action('delete', ctx => ctx.deleteMessage());
 const secretPath = `/telegraf/${bot.secretPathComponent()}`;
 
 // Set telegram webhook
@@ -42,7 +52,7 @@ app.get('/', (_: Request, res: Response) =>
 );
 
 // Set the bot API endpoint
-//app.use(bot.webhookCallback(secretPath));
+app.use(bot.webhookCallback(secretPath));
 
 app.listen(port, () => {
   console.log(`EWBot listening on port ${port}!`);
